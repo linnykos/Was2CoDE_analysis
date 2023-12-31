b9577b834b06fe710130da643273dfd36280b34f
Kevin Lin <kzlin@uw.edu>
2023-12-31 15:33:59 -0500

    Pushing the eSVD code

--- code/kevin/Writeup1/Writeup1_nebula_volcano.R
+++ code/kevin/Writeup1/Writeup1_nebula_volcano.R
@@ -7,7 +7,27 @@ load("~/lab/projects/subject-de/out/kevin/Writeup1/Writeup1_nebula.RData")
 head(nebula_res$summary)
 res <- nebula_res$summary
 
-plot1 <-  EnhancedVolcano::EnhancedVolcano(res,
-                                           lab = rownames(res),
-                                           x = 'log2FoldChange',
-                                           y = 'pvalue')
+pval_vec <- res[,"p_CognitiveStatusNo dementia"]
+pval_adj_vec <- stats::p.adjust(pval_vec, method = "BH")
+idx <- which(pval_adj_vec <= 0.05)
+pCutoff <- max(pval_vec[idx])
+FCcutoff <- quantile(abs(res[,"logFC_CognitiveStatusNo dementia"]), probs = 0.9)
+xlim <- c(-1,1) * quantile(abs(res[,"logFC_CognitiveStatusNo dementia"]), probs = 0.99)
+
+idx <- which(abs(res[,"logFC_CognitiveStatusNo dementia"]) <= max(xlim))
+ylim <- c(0, max(-log10(pval_vec[idx])))
+
+plot1 <-  EnhancedVolcano::EnhancedVolcano(
+  res,
+  lab = res$gene,
+  x = "logFC_CognitiveStatusNo dementia",
+  y = 'p_CognitiveStatusNo dementia',
+  pCutoff = pCutoff,
+  FCcutoff = FCcutoff,
+  xlim = xlim,
+  ylim = ylim
+)
+
+ggplot2::ggsave(filename = "../../../figures/kevin/Writeup1/Writeup1_nebula_volcano.png",
+                plot1, device = "png", width = 7, height = 7, units = "in")
+
