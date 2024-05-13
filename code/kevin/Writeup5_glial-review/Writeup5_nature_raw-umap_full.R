0c1340bb5eb3f6513b733ebe4f09924482fe074f
linnykos <kzlin@uw.edu>
2024-05-12 21:36:45 -0700

    Pushing a bunch of small changes

---
+++ code/kevin/Writeup5_glial-review/Writeup5_nature_raw-umap_full.R
@@ -0,0 +1,53 @@
+rm(list=ls())
+library(Seurat)
+
+load("~/kzlinlab/data/microglia-prater-2023/Prater_Green_PU1_MGsubset_10clusters_DeID.rdata")
+
+date_of_run <- Sys.time()
+session_info <- devtools::session_info()
+ss_data_norm <- Seurat::UpdateSeuratObject(ss_data_norm)
+
+metadf <- ss_data_norm@meta.data
+table(metadf[,"Pt_ID"], metadf[,"SeqBatch"])
+table(metadf[,"Study_Designation"], metadf[,"SeqBatch"])
+table(metadf[,"Pt_ID"], metadf[,"Study_Designation"])
+
+Seurat::DefaultAssay(ss_data_norm) <- "RNA"
+ss_data_norm[["integrated"]] <- NULL
+
+seurat_obj <- ss_data_norm
+rm(ls = "ss_data_norm"); gc(TRUE)
+
+# https://github.com/satijalab/seurat/issues/8183
+# https://github.com/satijalab/seurat/issues/7501#issuecomment-1854571904
+print("Performing SCTransform")
+set.seed(10)
+unwanted <- c("percent.mito")
+nfeatures <- 2000
+seurat_obj <- Seurat::SCTransform(seurat_obj, 
+                                  variable.features.n = nfeatures,
+                                  do.scale = FALSE,
+                                  do.center = FALSE,
+                                  conserve.memory = TRUE,
+                                  return.only.var.genes = FALSE,
+                                  vars.to.regress = unwanted,
+                                  verbose = FALSE)
+
+print("Performing PCA")
+set.seed(10)
+seurat_obj <- Seurat::RunPCA(seurat_obj, 
+                             features = Seurat::VariableFeatures(seurat_obj), 
+                             npcs = 50,
+                             verbose = FALSE)
+
+print("Performing UMAP")
+set.seed(10)
+seurat_obj <- Seurat::RunUMAP(object = seurat_obj, dims = 1:30)
+seurat_umap <- seurat_obj[["umap"]]
+
+save(seurat_umap, date_of_run, session_info,
+     file = "~/kzlinlab/projects/subject-de/out/kevin/Writeup4/Writeup4_nature_raw-umap_full.RData")
+
+
+
+
