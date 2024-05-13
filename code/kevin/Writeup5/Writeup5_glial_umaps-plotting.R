8b9eadd63b484a7d7788f19e2123e311d14209dd
linnykos <kzlin@uw.edu>
2024-05-12 19:57:33 -0700

    Pushing the updated palette

--- code/kevin/Writeup5/Writeup5_glial_umaps-plotting.R
+++ code/kevin/Writeup5/Writeup5_glial_umaps-plotting.R
@@ -41,3 +41,55 @@ for(i in 1:10){
                   height = 5, width = 5,
                   units = "in")
 }
+
+############################
+
+load("~/kzlinlab/projects/subject-de/out/kevin/Writeup5/Writeup5_glial_umaps_alt1.RData")
+
+for(i in 1:10){
+  print(paste("Working on trial:", i))
+  set.seed(i)
+  seurat_all[["umap"]] <- umap_list[[i]]
+  
+  plot1 <- Seurat::DimPlot(seurat_all, 
+                           reduction = "umap",
+                           label = TRUE,
+                           repel = TRUE,
+                           label.size = 2.5,
+                           group.by = "Supertype2",
+                           cols = col_palette)
+  plot1 <- plot1 + Seurat::NoLegend()
+  plot1 <- plot1 + ggplot2::ggtitle(paste0("UMAP, Seed: ", i))
+  plot1 <- plot1 + ggplot2::theme(legend.text = ggplot2::element_text(size = 5))
+  
+  ggplot2::ggsave(plot1, 
+                  file = paste0("~/kzlinlab/projects/subject-de/git/subject-de_kevin/figures/kevin/Writeup5/Writeup5_glial_umap_alt1-", i, ".png"),
+                  height = 5, width = 5,
+                  units = "in")
+}
+
+############################
+
+load("~/kzlinlab/projects/subject-de/out/kevin/Writeup5/Writeup5_glial_umaps_alt2.RData")
+
+for(i in 1:10){
+  print(paste("Working on trial:", i))
+  set.seed(i)
+  seurat_all[["umap"]] <- umap_list[[i]]
+  
+  plot1 <- Seurat::DimPlot(seurat_all, 
+                           reduction = "umap",
+                           label = TRUE,
+                           repel = TRUE,
+                           label.size = 2.5,
+                           group.by = "Supertype2",
+                           cols = col_palette)
+  plot1 <- plot1 + Seurat::NoLegend()
+  plot1 <- plot1 + ggplot2::ggtitle(paste0("UMAP, Seed: ", i))
+  plot1 <- plot1 + ggplot2::theme(legend.text = ggplot2::element_text(size = 5))
+  
+  ggplot2::ggsave(plot1, 
+                  file = paste0("~/kzlinlab/projects/subject-de/git/subject-de_kevin/figures/kevin/Writeup5/Writeup5_glial_umap_alt2-", i, ".png"),
+                  height = 5, width = 5,
+                  units = "in")
+}
