4018f7668727495c03e636d23e88e72ba22fc3cd
TatiZhang <Wenjiz1@uw.edu>
2024-07-02 18:07:28 -0700

    pushing some Writeup2 R files

--- code/tati/Writeup2/Writeup2_boxplots.R
+++ code/tati/Writeup2/Writeup2_boxplots.R
@@ -4,18 +4,13 @@ library(hrbrthemes)
 library(viridis)
 
 load("~/kzlinlab/projects/subject-de/out/tati/Writeup2/Writeup2_nebula.RData")
-
-# Extract the summary data frame
 nebula_summary <- nebula_res$summary
 
-# Define the significant genes from the volcano plot (example genes, replace with actual genes from your plot)
 significant_genes <- c("SORCS1", "DPYS", "ASTN1", "GLT1D1", "BRINP2", "PBX1", "MAPK10", "RP11-762E8.1", "FSTL5", "ARHGAP10")
 
-# Filter the summary data frame to include only significant genes
 filtered_data <- nebula_summary %>%
   filter(gene %in% significant_genes)
 
-# Create a boxplot for the significant genes
 plot1 <- ggplot(filtered_data, aes(x = gene, y = `logFC_(Intercept)`, fill = gene)) +
   geom_boxplot() +
   scale_fill_viridis(discrete = TRUE, alpha = 0.6) +
@@ -29,7 +24,6 @@ plot1 <- ggplot(filtered_data, aes(x = gene, y = `logFC_(Intercept)`, fill = gen
   ggtitle("Boxplot of Significant Genes Nebula") +
   xlab("Genes") +
   ylab("logFC (Intercept)")
-
 
 ggplot2::ggsave(filename = paste0("~/kzlinlab/projects/subject-de/git/subject-de_tati/figures/tati/Writeup2/Writeup2_nebula_boxplot.png"),
                 plot1, device = "png", width = 5, height = 7, units = "in")
