d222148f255967bbcf176c3d35d833ee514f315a
TatiZhang <Wenjiz1@uw.edu>
2024-07-02 18:23:02 -0700

    pushing some Writeup2 R files

--- code/tati/Writeup2/Writeup2_boxplots.R
+++ code/tati/Writeup2/Writeup2_boxplots.R
@@ -11,19 +11,25 @@ significant_genes <- c("SORCS1", "DPYS", "ASTN1", "GLT1D1", "BRINP2", "PBX1", "M
 filtered_data <- nebula_summary %>%
   filter(gene %in% significant_genes)
 
-plot1 <- ggplot(filtered_data, aes(x = gene, y = `logFC_(Intercept)`, fill = gene)) +
-  geom_boxplot() +
-  scale_fill_viridis(discrete = TRUE, alpha = 0.6) +
-  geom_jitter(color = "black", size = 0.4, alpha = 0.9) +
-  theme_ipsum() +
-  theme(
-    legend.position = "none",
-    plot.title = element_text(size = 11),
-    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)
-  ) +
-  ggtitle("Boxplot of Significant Genes Nebula") +
-  xlab("Genes") +
-  ylab("logFC (Intercept)")
+for (gene in significant_genes) {
+  # Filter the data for the current gene
+  gene_data <- nebula_summary %>%
+    filter(gene == !!gene)
+  
+  # Create the boxplot
+  plot1 <- ggplot(gene_data, aes(x = CognitiveStatus, y = `logFC_CognitiveStatusNo dementia`, fill = CognitiveStatus)) +
+    geom_boxplot() +
+    scale_fill_viridis(discrete = TRUE, alpha = 0.6) +
+    geom_jitter(color = "black", size = 0.4, alpha = 0.9) +
+    theme_ipsum() +
+    theme(
+      legend.position = "none",
+      plot.title = element_text(size = 11),
+      axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)
+    ) +
+    ggtitle(paste("Boxplot for Gene:", gene)) +
+    xlab("Cognitive Status") +
+    ylab("logFC (AD vs. non-AD)")
 
 ggplot2::ggsave(filename = paste0("~/kzlinlab/projects/subject-de/git/subject-de_tati/figures/tati/Writeup2/Writeup2_nebula_boxplot.png"),
                 plot1, device = "png", width = 5, height = 7, units = "in")
