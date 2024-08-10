456f60fb38c76832b3008082b6c3faa3cc6254e9
TatiZhang <Wenjiz1@uw.edu>
2024-08-09 17:17:12 -0700

    debugging

--- code/tati/Writeup3/Writeup3_Pseudobulk-DEseq2.R
+++ code/tati/Writeup3/Writeup3_Pseudobulk-DEseq2.R
@@ -1,31 +1,25 @@
 rm(list=ls())
+library(foreach)
+library(future)
+library(rngtools)
 library(Seurat)
-library(SummarizedExperiment)
-library(DESeq2)
-
-load("~/kzlinlab/projects/subject-de/out/kevin/Writeup6/Writeup6_prater_scvi-seurat.RData")
-
+library(nebula)
 set.seed(10)
-date_of_run <- Sys.time()
-session_info <- devtools::session_info()
 
-########
+print("Starting")
 
-tmp <- paste0("ID_", as.character(ss_data_norm$Pt_ID))
-ss_data_norm$Pt_ID <- factor(tmp)
-tmp <- ss_data_norm$CognitiveStatus  
-ss_data_norm$CognitiveStatus <- factor(gsub(
-  pattern = " ",
-  replacement = "_",
-  x = tmp
-))
+load("~/kzlinlab/projects/subject-de/out/kevin/preprocess/naive-preprocess.RData")
 
-#########
+gene_vec <- Seurat::VariableFeatures(ss_data_norm[["RNA"]])
+ss_data_norm <- subset(ss_data_norm, features = gene_vec)
 
 # adjust the covariates
 # need to convert to numeric
-categorical_vars <- c("Sex", "SeqBatch", "CognitiveStatus")
-numerical_vars <- c("PMI", "coded_Age")
+tmp <- paste0("ID_", as.character(ss_data_norm$Pt_ID))
+ss_data_norm$Pt_ID <- factor(tmp)
+
+categorical_vars <- c("Sex", "SeqBatch", "Race", "Study_Designation","genotype_APOE")
+numerical_vars <- c("PMI","coded_Age")
 
 tmp <- as.character(ss_data_norm$coded_Age)
 tmp[which(tmp == "90+")] <- "90"
@@ -38,84 +32,34 @@ for(variable in numerical_vars){
   ss_data_norm@meta.data[,variable] <- as.numeric(as.character(ss_data_norm@meta.data[,variable]))
 }
 
-# need to fill in NAs in PMI
+# need to fill in NAs in PMI,Race
 tab_list <- lapply(categorical_vars, function(variable){
   table(ss_data_norm$Pt_ID, ss_data_norm@meta.data[,variable])
 })
 names(tab_list) <- categorical_vars
 
-na_vars <- c("PMI")
-num_nas <- sapply(na_vars, function(variable){
-  tab_mat <- table(ss_data_norm$Pt_ID, ss_data_norm@meta.data[,variable])
-  length(which(rowSums(tab_mat) == 0))
-})
-na_vars <- na_vars[order(num_nas, decreasing = F)]
+pmi_missing_values <- c(ID_1 = 5.08,
+                        ID_10 = 3.33,
+                        ID_3 = 5.08,
+                        ID_15 = 6.97)
+for(i in 1:length(pmi_missing_values)){
+  subj_id <- names(pmi_missing_values)[i]
+  idx <- which(ss_data_norm$Pt_ID == subj_id)
+  ss_data_norm$PMI[idx] <- pmi_missing_values[i]
+}
 
-for(variable in na_vars){
-  tab_list <- lapply(categorical_vars, function(var_tmp){
-    table(ss_data_norm$Pt_ID, ss_data_norm@meta.data[,var_tmp])
-  })
-  names(tab_list) <- categorical_vars
-  
-  tab_mat <- table(ss_data_norm$Pt_ID, ss_data_norm@meta.data[,variable])
-  subj_to_match <- rownames(tab_mat)[which(rowSums(tab_mat) == 0)]
-  
-  # find all the categorical variables that match exactly
-  subj_paired <- lapply(subj_to_match, function(subj){
-    categorical_var_to_match <- setdiff(categorical_vars, variable)
-    possible_idx <- which(table(unlist(lapply(categorical_var_to_match, function(variable_tmp){
-      tab_mat <- tab_list[[variable_tmp]]
-      col_idx <- which(tab_mat[subj,] != 0)
-      idx <- which(tab_mat[,col_idx] != 0)
-      rownames(tab_mat)[idx]
-    }))) == length(categorical_var_to_match))
-    
-    possible_subj <- rownames(tab_list[[1]])[possible_idx]
-    possible_subj <- setdiff(possible_subj, subj)
-    stopifnot(length(possible_subj) > 0)
-    possible_subj
-  })
-  names(subj_paired) <- subj_to_match
-  
-  # among these subjects, find the ones that are closest in numerical variables
-  num_subjs <- length(subj_to_match)
-  subj_paired2 <- sapply(1:num_subjs, function(ii){
-    subj <- subj_to_match[ii]
-    possible_subj <- subj_paired[[ii]]
-    
-    numerical_var_to_match <- setdiff(numerical_vars, variable)
-    numerical_mat <- sapply(numerical_vars, function(var_tmp){
-      sapply(c(subj, possible_subj), function(subj_tmp){
-        tmp_idx <- which(ss_data_norm$Pt_ID == subj_tmp)
-        tmp <- unique(ss_data_norm@meta.data[tmp_idx,var_tmp])
-        stopifnot(length(tmp) == 1)
-        tmp
-      })
-    })
-    numerical_mat <- numerical_mat[,apply(numerical_mat, 2, function(x){!any(is.na(x))}), drop = F]
-    numerical_mat <- scale(numerical_mat)
-    dist_mat <- as.matrix(stats::dist(numerical_mat))
-    diag(dist_mat) <- Inf
-    rownames(dist_mat)[which.min(dist_mat[1,])]
-  })
-  names(subj_paired2) <- subj_to_match
-  
-  # now fill in values
-  subject_vec <- ss_data_norm$Pt_ID
-  zz <- ss_data_norm@meta.data[,variable]
-  for(ii in 1:num_subjs){
-    dest_idx <- which(subject_vec == names(subj_paired2)[ii])
-    source_idx <- which(subject_vec == subj_paired2[ii])
-    zz[dest_idx] <- unique(zz[source_idx])[1]
-  }
-  stopifnot(!any(is.na(zz)))
-  if(variable %in% categorical_vars) zz <- factor(zz)
-  ss_data_norm@meta.data[,variable] <- zz
+race_missing_values <- c(ID_5 = "White")
+for(i in 1:length(race_missing_values)){
+  subj_id <- names(race_missing_values)[i]
+  idx <- which(ss_data_norm$Pt_ID == subj_id)
+  ss_data_norm$Race[idx] <- race_missing_values[i]
 }
 
 zz <- ss_data_norm@meta.data[,c(categorical_vars, numerical_vars)]
 stopifnot(!any(is.na(zz)))
 summary(zz)
+
+
 
 ########
 
@@ -127,7 +71,7 @@ mat <- SeuratObject::LayerData(ss_data_norm,
                                layer = "counts",
                                features = Seurat::VariableFeatures(ss_data_norm))
 mat <- as.matrix(mat)
-categorical_var <- c("CognitiveStatus", "Pt_ID", "Sex", "SeqBatch")
+categorical_var <- c("Study_Designation", "Race","Pt_ID", "Sex", "SeqBatch", "genotype_APOE")
 numerical_var <- c("PMI", "coded_Age")
 metadata <- ss_data_norm@meta.data[,c(categorical_var, numerical_var)]
 for(var in categorical_var){
@@ -162,18 +106,18 @@ for(vr in numerical_var){
   metadata_pseudobulk[,vr] <- scale(metadata_pseudobulk[,vr])
 }
 
-metadata_pseudobulk[,"CognitiveStatus"] <- relevel(metadata_pseudobulk[,"CognitiveStatus"], ref = "No_dementia")
+metadata_pseudobulk[,"Study_Designation"] <- relevel(metadata_pseudobulk[,"Study_Designation"], ref = "No_dementia")
 
 dds <- DESeq2::DESeqDataSetFromMatrix(countData = mat_pseudobulk,
                                       colData = metadata_pseudobulk,
-                                      design = ~ Sex + SeqBatch + PMI + coded_Age + CognitiveStatus)
+                                      design = ~ Study_Designation + Sex + PMI + SeqBatch + coded_Age + genotype_APOE + Race)
 
 dds <- DESeq2::DESeq(dds)
 nms <- DESeq2::resultsNames(dds)
 deseq2_pval <- DESeq2::results(dds)$pvalue
 stats::quantile(deseq2_pval, na.rm = T)
 
-deseq2_res <- DESeq2::results(dds, name="CognitiveStatus_Dementia_vs_No_dementia")
+deseq2_res <- DESeq2::results(dds, name="Study_Designation_Dementia_vs_No_dementia")
 deseq2_res$padj_custom <- stats::p.adjust(deseq2_res[,"pvalue"], method = "BH")
 
 save(deseq2_res,
