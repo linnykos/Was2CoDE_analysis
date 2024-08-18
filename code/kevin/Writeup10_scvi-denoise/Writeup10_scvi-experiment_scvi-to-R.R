ef632083250f8278f8875bd7f0778102261fbeae
linnykos <kzlin@uw.edu>
2024-08-18 01:45:36 -0700

    Trying random things

---
+++ code/kevin/Writeup10_scvi-denoise/Writeup10_scvi-experiment_scvi-to-R.R
@@ -0,0 +1,109 @@
+rm(list=ls())
+library(Seurat)
+library(SeuratObject)
+library(SeuratDisk)
+library(arrow)
+
+load("~/kzlinlab/data/microglia-prater-2023/Prater_Green_PU1_MGsubset_10clusters_DeID.rdata")
+ss_data_norm <- Seurat::UpdateSeuratObject(ss_data_norm)
+
+# Reading the Feather file
+df <- arrow::read_feather("/home/users/kzlin/kzlinlab/projects/subject-de/out/kevin/Writeup10/Writeup10_scvi-experiment.feather")
+rowname_vec <- as.matrix(df[,ncol(df)])[,1]
+df <- df[,-ncol(df)]
+df <- as.matrix(df)
+rownames(df) <- rowname_vec
+
+Seurat::DefaultAssay(ss_data_norm) <- "RNA"
+ss_data_norm[["integrated"]] <- NULL
+
+# remove "scale.data" slot
+SeuratObject::LayerData(object = ss_data_norm, 
+                        assay = "RNA", 
+                        layer = "scale.data") <- NULL
+
+Seurat::VariableFeatures(ss_data_norm) <- colnames(df)
+ss_data_norm <- subset(ss_data_norm, features = Seurat::VariableFeatures(ss_data_norm))
+SeuratObject::LayerData(object = ss_data_norm, 
+                        assay = "RNA", 
+                        layer = "data") <- t(df)
+
+# compute PCA and UMAP
+ss_data_norm <- Seurat::ScaleData(ss_data_norm)
+ss_data_norm <- Seurat::RunPCA(ss_data_norm, 
+                               features = Seurat::VariableFeatures(ss_data_norm),
+                               verbose = FALSE)
+set.seed(10)
+ss_data_norm <- Seurat::RunUMAP(ss_data_norm, dims = 1:30)
+
+####################
+# basic preprocessing
+
+Seurat::DefaultAssay(ss_data_norm) <- "RNA"
+ss_data_norm[["RNA"]] <- as(object = ss_data_norm[["RNA"]], Class = "Assay5")
+
+# adjust the APOE meta-data column to avoid issues
+col_idx <- which(colnames(ss_data_norm@meta.data) == "APOE")
+colnames(ss_data_norm@meta.data)[col_idx] <- "genotype_APOE"
+
+# change the CognitiveStatus levels to avoid any issues
+ss_data_norm$CognitiveStatus <- factor(ss_data_norm$CognitiveStatus)
+level_vec <- levels(ss_data_norm$CognitiveStatus)
+level_vec[which(level_vec == "No dementia")] <- "No_dementia"
+levels(ss_data_norm$CognitiveStatus) <- level_vec
+
+# add letters to certain clusterings
+variable_names <- c("SeqBatch", 
+                    "seurat_clusters", 
+                    "integrated_snn_res.0.3", 
+                    "Pt_ID", 
+                    "orig.ident")
+letters_vec <- c("b", "c", "c", "pt", "pt")
+for(k in 1:length(variable_names)){
+  variable <- variable_names[k]
+  letter <- letters_vec[k]
+  ss_data_norm@meta.data[,variable] <- factor(paste0(letter, ss_data_norm@meta.data[,variable]))
+}
+
+# make things into their appropriate type
+categorical_vars <- c("Sex", 
+                      "Race", 
+                      "CognitiveStatus", 
+                      "genotype_APOE", 
+                      "Study_Designation")
+for(variable in categorical_vars){
+  ss_data_norm@meta.data[,variable] <- factor(ss_data_norm@meta.data[,variable])
+}
+summary(ss_data_norm@meta.data)
+
+#######################
+
+date_of_run <- Sys.time()
+session_info <- devtools::session_info()
+note <- "This is Katie's data but putting the scVI-normalized data (only 2000 genes) inside under 'data'."
+
+save(session_info, date_of_run, note,
+     ss_data_norm, 
+     file = "/home/users/kzlin/kzlinlab/projects/subject-de/out/kevin/Writeup6/Writeup6_prater_scvi-seurat.RData")
+
+#######################
+
+var_vec <- c("seurat_clusters", "Pt_ID", "CognitiveStatus", 
+             "Study_Designation", "Sex", "genotype_APOE",
+             "PMI", "BrainPh", "Race", "FreshBrainWeight",
+             "NIA_AA", "ThalPhase", 
+             "BraakStage", "CERAD", "LATEScore", "coded_Age",
+             "SeqBatch")
+
+pdf("~/kzlinlab/projects/subject-de/git/subject-de_kevin/figures/kevin/Writeup6/Writeup6_prater_scvi-seurat_umap-covariates.pdf",
+    onefile = T, width = 5, height = 5)
+
+for(variable in var_vec){
+  plot1 <- Seurat::DimPlot(ss_data_norm, 
+                           reduction = "umap",
+                           group.by = variable,
+                           raster = TRUE)
+  print(plot1)
+}
+
+dev.off()
--- code/kevin/Writeup6_scvi/Writeup6_eprs_supervised-pca.R
+++ code/kevin/Writeup6_scvi/Writeup6_eprs_supervised-pca.R
@@ -11,6 +11,13 @@ ss_data_norm <- Seurat::UpdateSeuratObject(ss_data_norm)
 colnames(ss_data_norm@meta.data)
 table(ss_data_norm$UWA, ss_data_norm$ePRS)
 
+metadata <- ss_data_norm@meta.data[,c("UWA", "ePRS", "age_at_death", "sex")]
+duplicated_idx <- which(duplicated(metadata$UWA))
+metadata <- metadata[-duplicated_idx,]
+metadata <- metadata[order(metadata$ePRS),]
+median(metadata$age_at_death[1:10])
+median(metadata$age_at_death[11:20])
+
 scvi_mat <- read.csv("~/kzlinlab/projects/subject-de/git/subject-de_kevin/csv/kevin/Writeup6/Writeup6_eprs_mg_scvi_scVI.csv")
 
 rownames(scvi_mat) <- scvi_mat[,1]
@@ -39,16 +46,11 @@ spca_res <- supervised_pca(
   k = 1
 )
 
-ss_data_norm[["sPCA"]] <- Seurat::CreateDimReducObject(embeddings = spca_res$dimred, 
-                                                       assay = "integrated")
+df <- spca_res$dimred
+df <- cbind(df, ss_data_norm@meta.data[,c("UWA", "ePRS")])
 
-plot1 <- Seurat::DimPlot(ss_data_norm, 
-                         reduction = "sPCA",
-                         group.by = "ePRS",
-                         raster = TRUE)
-ggplot2::ggsave(plot1,
-                filename = "~/kzlinlab/projects/subject-de/git/subject-de_kevin/figures/kevin/Writeup6/Writeup6_eprs_mg_scvi_sPCA_ePRS.png",
-                width = 8, height = 8)
+write.csv(df,
+          file = "~/kzlinlab/projects/subject-de/git/subject-de_kevin/csv/kevin/Writeup6/Writeup6_eprs_mg_sPCA-1.csv")
 
 ############
 
@@ -117,16 +119,51 @@ ggplot2::ggsave(plot1,
                 width = 8, height = 8)
 
 umap_res <- ss_data_norm[["scVI_UMAP"]]@cell.embeddings
+
+metadata <- ss_data_norm@meta.data
+tab_mat <- table(metadata$UWA, metadata$ePRS)
+donor_high <- rownames(tab_mat)[which(tab_mat[,"High"] != 0)]
+donor_low <- rownames(tab_mat)[which(tab_mat[,"Low"] != 0)]
+num_high <- length(donor_high)
+num_low <- length(donor_low)
+
+case_color_palette <- grDevices::colorRampPalette(c(rgb(255, 174, 0, maxColorValue = 255),
+                                                    rgb(255, 95, 75, maxColorValue = 255)))(num_high)
+control_color_palette <- grDevices::colorRampPalette(c(rgb(0, 122, 0, maxColorValue = 255),
+                                                       rgb(27, 65, 35, maxColorValue = 255)))(num_low)
+
+donor_colors <- c(case_color_palette, control_color_palette)
+names(donor_colors) <- c(donor_high, donor_low)
+
 n <- nrow(umap_res)
-color_vec <- rep("darkgreen", n)
-idx <- which(ss_data_norm$ePRS == "High")
-color_vec[idx] <- "orange"
+color_vec <- rep(NA, n)
+for(donor in names(donor_colors)){
+  idx <- which(metadata$UWA == donor)
+  color_vec[idx] <- donor_colors[donor]
+}
 
 set.seed(10)
 order_vec <- sample(n)
 
 png("~/kzlinlab/projects/subject-de/git/subject-de_kevin/figures/kevin/Writeup6/Writeup6_eprs_mg_scvi_ePRS_R-umap_full.png",
     height = 1200, width = 1500, units = "px", res = 300)
+par(mar = c(2, 2, 0.5, 0.5))
+plot(umap_res[order_vec,1],
+     umap_res[order_vec,2],
+     pch = 16,
+     cex = 0.2,
+     col = color_vec[order_vec],
+     xaxt = "n",
+     yaxt = "n",
+     xlab = "",
+     ylab = "",
+     bty = "n")
+axis(1); axis(2)
+graphics.off()
+
+
+png("~/kzlinlab/projects/subject-de/git/subject-de_kevin/figures/kevin/Writeup6/Writeup6_eprs_mg_scvi_ePRS_R-umap_full2.png",
+    height = 1500, width = 1200, units = "px", res = 300)
 par(mar = c(2, 2, 0.5, 0.5))
 plot(umap_res[order_vec,1],
      umap_res[order_vec,2],
