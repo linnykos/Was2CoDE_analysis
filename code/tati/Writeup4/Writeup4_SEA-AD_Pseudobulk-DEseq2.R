9417e2d3fed8de33f4a73e9f467587515dbb86cc
TatiZhang <Wenjiz1@uw.edu>
2024-08-20 16:48:59 -0700

    committing deseq2

--- code/tati/Writeup4/Writeup4_SEA-AD_Pseudobulk-DEseq2.R
+++ code/tati/Writeup4/Writeup4_SEA-AD_Pseudobulk-DEseq2.R
@@ -10,69 +10,77 @@ print("Starting")
 
 load("~/kzlinlab/projects/subject-de/out/kevin/Writeup2/Writeup2_sea-ad_microglia_preprocess.RData")
 
-gene_vec <- Seurat::VariableFeatures(ss_data_norm[["RNA"]])
-ss_data_norm <- subset(ss_data_norm, features = gene_vec)
-
-# adjust the covariates
-# need to convert to numeric
-tmp <- paste0("ID_", as.character(ss_data_norm$Pt_ID))
-ss_data_norm$Pt_ID <- factor(tmp)
-
-categorical_vars <- c("Sex", "SeqBatch", "Race", "Study_Designation","genotype_APOE")
-numerical_vars <- c("PMI","coded_Age")
-
-tmp <- as.character(ss_data_norm$coded_Age)
-tmp[which(tmp == "90+")] <- "90"
-ss_data_norm$coded_Age <- tmp
-
-for(variable in categorical_vars){
-  ss_data_norm@meta.data[,variable] <- factor(ss_data_norm@meta.data[,variable])
-}
-for(variable in numerical_vars){
-  ss_data_norm@meta.data[,variable] <- as.numeric(as.character(ss_data_norm@meta.data[,variable]))
-}
-
-# need to fill in NAs in PMI,Race
-tab_list <- lapply(categorical_vars, function(variable){
-  table(ss_data_norm$Pt_ID, ss_data_norm@meta.data[,variable])
+# construct the age vector
+age_vec <- sapply(seurat_obj$development_stage, function(x){
+  substr(x, start = 0, stop = 2)
 })
-names(tab_list) <- categorical_vars
 
-pmi_missing_values <- c(ID_1 = 5.08,
-                        ID_10 = 3.33,
-                        ID_3 = 5.08,
-                        ID_15 = 6.97)
-for(i in 1:length(pmi_missing_values)){
-  subj_id <- names(pmi_missing_values)[i]
-  idx <- which(ss_data_norm$Pt_ID == subj_id)
-  ss_data_norm$PMI[idx] <- pmi_missing_values[i]
+age_vec[which(seurat_obj$development_stage == "80yearoldandoverhumanstage")] <- "90"
+age_vec <- as.numeric(age_vec)
+seurat_obj$AgeAtDeath <- age_vec
+
+
+seurat_obj$Lewy.body.disease.pathology <- seurat_obj@meta.data[,"Lewy body disease pathology"]
+seurat_obj$APOE4.status <- seurat_obj@meta.data[,"APOE4 status"]
+#colnames(seurat_obj@meta.data)
+
+# gene_vec <- Seurat::VariableFeatures(seurat_obj@meta.data[["RNA"]])
+# seurat_obj@meta.data <- subset(seurat_obj@meta.data, features = gene_vec)
+
+tmp <- paste0("ID_", as.character(seurat_obj@meta.data$donor_id))
+seurat_obj$donor_id <- factor(tmp)
+# Remove "Reference" donors using subset function
+seurat_obj <- subset(seurat_obj, subset = ADNC != "Reference")
+
+# Reclassify ADNC levels
+seurat_obj$ADNC <- with(seurat_obj@meta.data, 
+                        ifelse(ADNC %in% c("NotAD", "Low"), "Control", 
+                               ifelse(ADNC %in% c("Intermediate", "High"), "Case", ADNC)))
+
+# table(seurat_obj$donor_id, seurat_obj$ADNC)
+
+# Convert APOE4 status to binary (0 for "N", 1 for "Y")
+seurat_obj$APOE4_status <- ifelse(seurat_obj@meta.data$APOE4.status == "Y", 1, 
+                                  ifelse(seurat_obj@meta.data$APOE4.status == "N", 0, NA))
+
+# table(seurat_obj$donor_id, seurat_obj$APOE4_status)
+# Convert PMI Categories to (Numeric or) Factor
+seurat_obj$PMI <- factor(seurat_obj$PMI, levels = c("32to59hours", "59to87hours", "87to114hours"))
+
+categorical_vars <- c("ADNC", "sex", "assay", "self_reported_ethnicity","APOE4_status","PMI")
+numerical_vars <- c("AgeAtDeath")
+
+tmp <- as.character(seurat_obj$AgeAtDeath)
+tmp[which(tmp == "90+")] <- "90"
+seurat_obj@meta.data$AgeAtDeath <- tmp
+
+
+for (variable in categorical_vars) {
+  seurat_obj@meta.data[, variable] <- factor(seurat_obj@meta.data[, variable])
 }
 
-race_missing_values <- c(ID_5 = "White")
-for(i in 1:length(race_missing_values)){
-  subj_id <- names(race_missing_values)[i]
-  idx <- which(ss_data_norm$Pt_ID == subj_id)
-  ss_data_norm$Race[idx] <- race_missing_values[i]
+for (variable in numerical_vars) {
+  seurat_obj@meta.data[, variable] <- as.numeric(as.character(seurat_obj@meta.data[, variable]))
 }
 
-zz <- ss_data_norm@meta.data[,c(categorical_vars, numerical_vars)]
+zz <- seurat_obj@meta.data[,c(categorical_vars, numerical_vars)]
 stopifnot(!any(is.na(zz)))
 summary(zz)
-
-
+###########
+seurat_obj <- Seurat::FindVariableFeatures(seurat_obj, selection.method = "vst", nfeatures = 3000)
+gene_vec <- Seurat::VariableFeatures(seurat_obj[["RNA"]])
+seurat_obj <- subset(seurat_obj, features = gene_vec)
 
 ########
 
-subj_vec <- ss_data_norm$Pt_ID
+subj_vec <- seurat_obj$donor_id
 
 Seurat::DefaultAssay(ss_data_norm) <- "RNA"
 mat <- SeuratObject::LayerData(ss_data_norm, 
                                assay = "RNA", 
                                layer = "counts",
-                               features = Seurat::VariableFeatures(ss_data_norm))
+                               features = Seurat::VariableFeatures(seurat_obj))
 mat <- as.matrix(mat)
-categorical_var <- c("Study_Designation", "Race","Pt_ID", "Sex", "SeqBatch", "genotype_APOE")
-numerical_var <- c("PMI", "coded_Age")
 metadata <- ss_data_norm@meta.data[,c(categorical_var, numerical_var)]
 for(var in categorical_var){
   metadata[,var] <- as.factor(metadata[,var])
@@ -106,20 +114,20 @@ for(vr in numerical_var){
   metadata_pseudobulk[,vr] <- scale(metadata_pseudobulk[,vr])
 }
 
-metadata_pseudobulk[,"Study_Designation"] <- relevel(metadata_pseudobulk[,"Study_Designation"], ref = "Ctrl")
+metadata_pseudobulk[,"ADNC"] <- relevel(metadata_pseudobulk[,"ADNC"], ref = "Control")
 
 dds <- DESeq2::DESeqDataSetFromMatrix(countData = mat_pseudobulk,
                                       colData = metadata_pseudobulk,
-                                      design = ~ Study_Designation + Sex + PMI + SeqBatch + coded_Age + genotype_APOE + Race)
+                                      design = ~ ADNC + sex + PMI + assay + AgeAtDeath + APOE4_status + self_reported_ethnicity)
 
 dds <- DESeq2::DESeq(dds)
 nms <- DESeq2::resultsNames(dds)
 deseq2_pval <- DESeq2::results(dds)$pvalue
 stats::quantile(deseq2_pval, na.rm = T)
 
-deseq2_res <- DESeq2::results(dds, name="Study_Designation_AD_vs_Ctrl")
+deseq2_res <- DESeq2::results(dds, name="ADNC_Case_vs_Control")
 deseq2_res$padj_custom <- stats::p.adjust(deseq2_res[,"pvalue"], method = "BH")
 
 save(deseq2_res,
      date_of_run, session_info,
-     file = "~/kzlinlab/projects/subject-de/out/tati/Writeup3/Writeup3_Katie_Pseudobulk-DEseq2.RData")
+     file = "~/kzlinlab/projects/subject-de/out/tati/Writeup4/Writeup4_SEA-AD_Pseudobulk-DEseq2.RData")
