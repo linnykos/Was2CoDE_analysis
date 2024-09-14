93ec5e3d20971e30cbcc87fe7894da1c53b7dedd
TatiZhang <Wenjiz1@uw.edu>
2024-09-14 11:30:02 -0700

    updating code

--- code/tati/Writeup5/gsea_experiment.R
+++ code/tati/Writeup5/gsea_experiment.R
@@ -1,7 +1,8 @@
 load("~/kzlinlab/projects/subject-de/out/tati/Writeup5/Writeup5_prater_NEBULA.RData")
 head(nebula_res$summary)
 
-teststat_vec <- -log10(nebula_res$summary[,"p_Study_DesignationAD"]) # our "proxy" for a test-statistics
+# teststat_vec <- -log10(nebula_res$summary[,"p_Study_DesignationAD"]) # our "proxy" for a test-statistics
+teststat_vec <- nebula_res$summary[,"logFC_Study_DesignationAD"]
 names(teststat_vec) <- nebula_res$summary[,"gene"]
 
 # BiocManager::install("org.Hs.eg.db")
@@ -13,12 +14,45 @@ names(teststat_vec) <- nebula_res$summary[,"gene"]
 library(org.Hs.eg.db)
 library(clusterProfiler)
 
+teststat_vec <- sort(teststat_vec, decreasing = TRUE)
+
+# # Convert symbols to check if they map correctly to gene IDs
+# mapped_genes <- clusterProfiler::bitr(names(teststat_vec), 
+#                                       fromType = "SYMBOL", 
+#                                       toType = "ENTREZID", 
+#                                       OrgDb = org.Hs.eg.db)
+# 
+# # Filter teststat_vec to keep only the mapped genes
+# teststat_vec_mapped <- teststat_vec[names(teststat_vec) %in% mapped_genes$SYMBOL]
+# 
+# # Replace gene symbols with ENTREZ IDs
+# names(teststat_vec_mapped) <- mapped_genes$ENTREZID[match(names(teststat_vec_mapped), mapped_genes$SYMBOL)]
+# 
+# # Sort the updated vector (optional if needed)
+# teststat_vec_mapped <- sort(teststat_vec_mapped, decreasing = TRUE)
+# head(teststat_vec_mapped)
+
+# https://www.youtube.com/watch?v=Mi6u4r0lJvo
+set.seed(10)
 gse <- clusterProfiler::gseGO(
   teststat_vec,
   ont = "BP", # what kind of pathways are you interested in
-  keytype = "SYMBOL",
+  keyType = "SYMBOL",
   OrgDb = "org.Hs.eg.db",
   pvalueCutoff = 0.05,       # p-value threshold for pathways
   minGSSize = 10,            # minimum gene set size
   maxGSSize = 500            # maximum gene set size
 )
+
+head(as.data.frame(gse))
+
+plot1 <- enrichplot::dotplot(gse, showCategory=30) + ggplot2::ggtitle("dotplot for GSEA (Katie's NEBULA)")
+plot1 <- plot1 + ggplot2::theme(axis.text.y = ggplot2::element_text(size = 8))
+
+ggplot2::ggsave(filename = paste0("~/kzlinlab/projects/subject-de/git/subject-de_tati/figures/tati/Writeup5/Writeup5_prater_nebula-test.png"),
+                plot1, device = "png", width = 7, height = 7, units = "in")
+
+# https://yulab-smu.top/biomedical-knowledge-mining-book/enrichplot.html?q=gsea#ridgeline-plot-for-expression-distribution-of-gsea-result
+# clusterProfiler::dotplot(gse, showCategory=30) + ggtitle("dotplot for GSEA")
+
+
--- code/tati/Writeup7/Writeup7_ROSMAP_NEBULA.R
+++ code/tati/Writeup7/Writeup7_ROSMAP_NEBULA.R
@@ -12,18 +12,18 @@ load("~/kzlinlab/projects/subject-de/out/kevin/Writeup11/Writeup11_rosmap_scVI-p
 # head(seurat_obj@meta.data)
 
 # construct the age vector
-age_vec <- as.numeric(seurat_obj$age_death)
-seurat_obj$age_death <- age_vec
-seurat_obj$APOEe4_status <- seurat_obj@meta.data[,"APOEe4_status"]
+# age_vec <- as.numeric(seurat_obj$age_death)
+# seurat_obj$age_death <- age_vec
+# seurat_obj$APOEe4_status <- seurat_obj@meta.data[,"APOEe4_status"]
 #colnames(seurat_obj@meta.data)
 
 # gene_vec <- Seurat::VariableFeatures(seurat_obj@meta.data[["RNA"]])
 
-tmp <- paste0("ID_", as.character(seurat_obj@meta.data$Pt_ID))
-seurat_obj$Pt_ID <- factor(tmp)
+# tmp <- paste0("ID_", as.character(seurat_obj@meta.data$Pt_ID))
+# seurat_obj$Pt_ID <- factor(tmp)
 
 # Reclassify ADpath levels
-seurat_obj$ADpath <- factor(ifelse(seurat_obj$ADpath == "yes", "Case", "Control"))
+# seurat_obj$ADpath <- factor(ifelse(seurat_obj$ADpath == "yes", "Case", "Control"))
 # summary(seurat_obj$ADpath)
 
 # table(seurat_obj$Pt_ID, seurat_obj$ADpath)
@@ -32,21 +32,21 @@ seurat_obj$ADpath <- factor(ifelse(seurat_obj$ADpath == "yes", "Case", "Control"
 
 categorical_vars <- c("ADpath", "sex", "race", "batch", "APOEe4_status")
 numerical_vars <- c("age_death", "pmi")
-
-for (variable in categorical_vars) {
-  seurat_obj@meta.data[, variable] <- factor(seurat_obj@meta.data[, variable])
-}
-
-for (variable in numerical_vars) {
-  seurat_obj@meta.data[, variable] <- as.numeric(as.character(seurat_obj@meta.data[, variable]))
-}
+# 
+# for (variable in categorical_vars) {
+#   seurat_obj@meta.data[, variable] <- factor(seurat_obj@meta.data[, variable])
+# }
+# 
+# for (variable in numerical_vars) {
+#   seurat_obj@meta.data[, variable] <- as.numeric(as.character(seurat_obj@meta.data[, variable]))
+# }
 
 zz <- seurat_obj@meta.data[,c(categorical_vars, numerical_vars)]
 stopifnot(!any(is.na(zz)))
 summary(zz)
 ###########
-gene_vec <- Seurat::VariableFeatures(seurat_obj[["RNA"]])
-seurat_obj <- subset(seurat_obj, features = gene_vec)
+# gene_vec <- Seurat::VariableFeatures(seurat_obj[["RNA"]])
+# seurat_obj <- subset(seurat_obj, features = gene_vec)
 
 ##########################
 
@@ -64,7 +64,7 @@ neb_data$id <- neb_data$id[order_index]
 neb_data$pred <- neb_data$pred[order_index, ]
 neb_data$offset <- neb_data$offset[order_index]
 
-df <- model.matrix( ~ ADpath + sex + PMI + assay + age_death + APOEe4_status + self_reported_ethnicity,
+df <- model.matrix( ~ ADpath + sex + pmi + batch + age_death + APOEe4_status + race,
                     data = neb_data$pred)
 start_time <- Sys.time()
 nebula_res <- nebula::nebula(count = neb_data$count,
