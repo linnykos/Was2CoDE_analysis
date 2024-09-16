2d996c29bec97e5d59cbcb045d3dec05706bd8cc
TatiZhang <Wenjiz1@uw.edu>
2024-09-16 01:39:09 -0700

    updating seaad esvd

--- code/tati/Writeup6/Writeup6_SEA-AD_eSVD.R
+++ code/tati/Writeup6/Writeup6_SEA-AD_eSVD.R
@@ -11,64 +11,18 @@ print("Starting")
 
 load("~/kzlinlab/projects/subject-de/out/kevin/Writeup10/Writeup10_sea-ad_microglia_scVI-postprocessed.RData") 
 # the loaded dataset is called "ss_data_norm"
+categorical_vars <- c("ADNC", "sex", "assay", "self_reported_ethnicity", "APOE4status")
+numerical_vars <- c("Ageatdeath", "PMI")
 
-# construct the age vector
-age_vec <- sapply(seurat_obj$development_stage, function(x){
-  substr(x, start = 0, stop = 2)
-})
-
-age_vec[which(seurat_obj$development_stage == "80yearoldandoverhumanstage")] <- "90"
-age_vec <- as.numeric(age_vec)
-seurat_obj$Ageatdeath <- age_vec
-
-
-seurat_obj$Lewy.body.disease.pathology <- seurat_obj@meta.data[,"Lewybodydiseasepathology"]
-seurat_obj$APOE4.status <- seurat_obj@meta.data[,"APOE4status"]
-#colnames(seurat_obj@meta.data)
-
-# gene_vec <- Seurat::VariableFeatures(seurat_obj@meta.data[["RNA"]])
-
-tmp <- paste0("ID_", as.character(seurat_obj@meta.data$donor_id))
-seurat_obj$donor_id <- factor(tmp)
-# Remove "Reference" donors using subset function
-seurat_obj <- subset(seurat_obj, subset = ADNC != "Reference")
-
-# Reclassify ADNC levels
-seurat_obj$ADNC <- with(seurat_obj@meta.data, 
-                        ifelse(ADNC %in% c("NotAD", "Low"), "Control", 
-                               ifelse(ADNC %in% c("Intermediate", "High"), "Case", ADNC)))
-
-# table(seurat_obj$donor_id, seurat_obj$ADNC)
-
-# Convert APOE4 status to binary (0 for "N", 1 for "Y")
-seurat_obj$APOE4_status <- ifelse(seurat_obj@meta.data$APOE4.status == "Y", 1, 
-                                  ifelse(seurat_obj@meta.data$APOE4.status == "N", 0, NA))
-
-# table(seurat_obj$donor_id, seurat_obj$APOE4_status)
-
-categorical_vars <- c("ADNC", "sex", "assay", "self_reported_ethnicity","APOE4_status")
-numerical_vars <- c("Ageatdeath","PMI")
-
-tmp <- as.character(seurat_obj$Ageatdeath)
-seurat_obj@meta.data$Ageatdeath <- tmp
-
-
-for (variable in categorical_vars) {
-  seurat_obj@meta.data[, variable] <- factor(seurat_obj@meta.data[, variable])
-}
-
-for (variable in numerical_vars) {
-  seurat_obj@meta.data[, variable] <- as.numeric(as.character(seurat_obj@meta.data[, variable]))
-}
-
-zz <- seurat_obj@meta.data[,c(categorical_vars, numerical_vars)]
+# Now extract the data
+zz <- seurat_obj@meta.data[, c(categorical_vars, numerical_vars)]
 stopifnot(!any(is.na(zz)))
 summary(zz)
 
 ###########
 
-gene_vec <- Seurat::VariableFeatures(seurat_obj[["RNA"]])
-seurat_obj <- subset(seurat_obj, features = gene_vec)
+# gene_vec <- Seurat::VariableFeatures(seurat_obj[["RNA"]])
+# seurat_obj <- subset(seurat_obj, features = gene_vec)
 
 mat <- Matrix::t(SeuratObject::LayerData(seurat_obj, 
                                          assay = "RNA", 
@@ -105,7 +59,7 @@ eSVD_obj <- eSVD2::initialize_esvd(dat = mat,
                                    verbose = 1)
 time_end1 <- Sys.time()
 
-omitted_variables <- colnames(eSVD_obj$covariates)[grep("SeqBatch", colnames(eSVD_obj$covariates))]
+omitted_variables <- colnames(eSVD_obj$covariates)[grep("assay", colnames(eSVD_obj$covariates))]
 eSVD_obj <- eSVD2::reparameterization_esvd_covariates(
   input_obj = eSVD_obj,
   fit_name = "fit_Init",
